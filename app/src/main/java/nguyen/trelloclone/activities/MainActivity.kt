561b55b112b91ba654b28229d6c64bffbbc51a9d
Tran Phu Nguyen <tranphunguyen.fit@gmail.com>
2020-05-18 18:11:14 +0700

    feat(MainActivity): added Drawer Functionality

--- app/src/main/java/nguyen/trelloclone/activities/MainActivity.kt
+++ app/src/main/java/nguyen/trelloclone/activities/MainActivity.kt
@@ -1,13 +1,63 @@
 package nguyen.trelloclone.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 import nguyen.trelloclone.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpActionBar()
+
+        nav_view.setNavigationItemSelectedListener {menu ->
+            when(menu.itemId) {
+                R.id.nav_my_profile -> {
+                    Toast.makeText(this@MainActivity, "Profile", Toast.LENGTH_LONG).show()
+                }
+                R.id.nav_sign_out -> {
+                    FirebaseAuth.getInstance().signOut()
+
+                    val intent = Intent(this, IntroActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+            true
+        }
+    }
+
+    override fun onBackPressed() {
+        if(drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            doubleBackToExit()
+        }
+    }
+
+    private fun setUpActionBar() {
+        setSupportActionBar(toolbar_main_activity)
+        toolbar_main_activity.setNavigationIcon(R.drawable.ic_action_navigation_menu_24dp)
+        toolbar_main_activity.setNavigationOnClickListener {
+            toggleDrawer()
+        }
+    }
+
+    private fun toggleDrawer() {
+        if(drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            drawer_layout.openDrawer(GravityCompat.START)
+        }
     }
 }
